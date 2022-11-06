b40fe2c17c81b3688fc1c60d1c7f6d85f7d8f087
Luis Peralta <luisperalta182@gmail.com>
2022-11-06 17:11:54 -0600

    adding changes

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# Getting Started with Create React App
+# WeatherAPI Project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<div id="header" align="center">
+  <img src="https://res.cloudinary.com/dkeawhrem/image/upload/v1667775224/Screenshot_2022-11-06_at_16.50.55_pkfgos.png" width="100"/>
+</div>
 
-## Available Scripts
+## Used Technologies
 
-In the project directory, you can run:
+To develop this project the following technologies were used:
 
-### `npm start`
+<div>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width="40" height="40" /> &nbsp;
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" width="40" height="40" /> &nbsp;
+</div>
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Project running on the following link
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
 
-### `npm test`
+## Design taken from dribble
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[Check the Design](https://dribbble.com/shots/7767460-Weather-App-Website)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { IoMdSunny, IoMdRainy, IoMdCloudy, IoMdSnow, IoMdThunderstorm, IoMdSearch } from 'react-icons/io';
+import { BsCloudHaze2Fill, BsCloudDrizzleFill } from 'react-icons/bs';
+import { ImSpinner8 } from 'react-icons/im';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const APIkey = '1cdbec9cc466bdf4221edbda0f5e4914';
+
+
+const App = () => {
+
+    const [location, setLocation] = useState('Nicaragua');
+    const [data, setData] = useState(null);
+    const [inputValue, setInputValue] = useState('');
+    const [animate, setAnimate] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(false);
+
+    const handleInput = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        // console.log(inputValue);
+        if(inputValue !== ''){
+            setLocation(inputValue);
+        } else {
+            setAnimate(true);
+        }
+
+        const input = document.querySelector('input');
+        input.value = '';
+
+        e.preventDefault();
+    }
+
+    const handleCity = (e) => {
+        setLocation(e);
+    }
+
+    useEffect(() => {
+
+        setLoading(true);
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
+
+        axios.get(url).then(res => {
+            setTimeout(() => {
+                // setAnimate(false);
+                setData(res.data);
+                setLoading(false);
+            }, 1500)
+        }).catch(err=> {
+            setLoading(false);
+            setErrorMsg(err);
+        })
+    }, [location]);
+
+    useEffect(()=> {
+        const timer = setTimeout(()=> {
+            setErrorMsg('')
+        }, 2000)
+        return()=> clearTimeout(timer);
+    }, [errorMsg]);
+
+    let icon;
+
+    // console.log(data?.weather[0]?.main)
+
+    switch(data?.weather[0]?.main){
+        case 'Clouds':
+            icon = <IoMdCloudy />
+            break;
+        case 'Haze':
+            icon = <BsCloudHaze2Fill />
+            break;
+        case 'Rain':
+            icon = <IoMdRainy />
+            break;
+        case 'Clear':
+            icon = <IoMdSunny />
+            break;
+        case 'Drizzle':
+            icon = <BsCloudDrizzleFill />
+            break;
+        case 'Snow':
+            icon = <IoMdSnow />
+            break;
+        case 'Thunderstorm':
+            icon = <IoMdThunderstorm />
+            break;
+    };
+
+    if (!data){
+        return(
+            <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center'>
+                <div>
+                    <ImSpinner8 className='text-5xl animate-spin text-white' />
+                </div>
+            </div>
+        )
+    }
+
+    console.log(data);
+
+    const date = new Date();
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const month = ["Jan","Feb","March","April","May","June","July","August","Sept","Oct","Nov","Dec"];
+
+  return(
+    <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center lg:bg-[55%_90%] lg:bg-[length:150%] flex flex-col lg:flex-row lg:ustify-between lg:items-end'>
+        {errorMsg && <div className='w-full max-w-[90vw] lg:max-w-[450px] text-white'>{`${errorMsg.response.data.message}`}</div>}
+        <div className='pb-[100px] pt-[50px]  lg:pt-[0px] lg:px-10'>
+            <div className='flex flex-col lg:flex-row items-center'>
+                <div className='flex flex-cols lg:flex-row px-[30px]'>
+                    <p className='text-white text-9xl'>{parseInt(data.main.temp)}°</p>
+                    {/* <WiDegrees  className='text-white max-w-7xl	text-[130px]'/> */}
+                </div>
+                <div className='flex flex-col lg:flex-row justify-center items-center'>
+                    <div>
+                        <p className='text-white text-[50px] leading-none'>{data.name}</p>
+                        <p className='text-white text-[16px] font-light pt-[5px]'>{date.getUTCHours()}:{date.getUTCMinutes()} - {weekday[date.getUTCDate()]}, {month[date.getUTCMonth()]} {date.getUTCFullYear()}</p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center px-[30px]'>
+                        <p className='text-white text-[50px] text-center'>{icon}</p>
+                        <p className='text-white text-[16px] font-light text-center'>{data.weather[0].main}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='bg-[#356388]/[0.6] w-auto lg:absolute top-0 right-0 lg:w-[450px] lg:h-screen pt-[30px] px-[40px]'>
+            <form className={`
+            ${
+                animate ? 'animate-shake' : 'animate-none'
+            }
+            `}>
+                <div className='h-full relative flex items-center justify-between'>
+                    <input onChange={(e) => handleInput(e)} className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[16px] font-light h-full' type="text" placeholder='Search by City or Country' />
+                    <button onClick={(e) => handleSubmit(e)} className='bg-transparent w-20 h-12 flex justify-center items-center transition'><IoMdSearch className='text-2xl text-white' /></button>
+                </div>
+            </form>
+            <div className='bg-white/[0.6] h-[1px] mt-[20px] mb-[40px]'></div>
+            <div className='hidden lg:block'>
+                <p className='text-white pb-[15px]'>Other Cities</p>
+                <div className='py-[20px]'>
+                    <p onClick={(e) => handleCity('Managua')} className='cursor-pointer text-white/[0.6] capitalize'>Managua</p>
+                </div>
+                <div className='py-[20px]'>
+                    <p onClick={(e) => handleCity('San Jose, CR')} className='cursor-pointer text-white/[0.6] capitalize'>San Jose, CR</p>
+                </div>
+                <div className='py-[20px]'>
+                    <p onClick={(e) => handleCity('Guatemala City')} className='cursor-pointer text-white/[0.6] capitalize'>Guatemala City</p>
+                </div>
+                <div className='py-[20px]'>
+                    <p onClick={(e) => handleCity('Tegucigalpa')} className='cursor-pointer text-white/[0.6] capitalize'>Tegucigalpa</p>
+                </div>
+            </div>
+
+            <div className='bg-white/[0.6] h-[1px] mt-[20px] mb-[40px] hidden lg:block'></div>
+            <p className='text-white pb-[15px]'>Weather Details</p>
+            <div>
+                <div className='flex flex-row justify-between py-[20px]'>
+                    <p className='text-white/[0.6]'>Clouds</p>
+                    <p className='text-white'>{data.visibility / 1000}km</p>
+                </div>
+                <div className='flex flex-row justify-between py-[20px]'>
+                    <p className='text-white/[0.6]'>Humidity</p>
+                    <p className='text-white'>{data.main.humidity}%</p>
+                </div>
+                <div className='flex flex-row justify-between py-[20px]'>
+                    <p className='text-white/[0.6]'>Wind</p>
+                    <p className='text-white'>{data.wind.speed}m/s</p>
+                </div>
+                <div className='flex flex-row justify-between py-[20px]'>
+                    <p className='text-white/[0.6]'>Feels like</p>
+                    <p className='text-white'>{parseInt(data.main.feels_like)}°</p>
+                </div>
+            </div>
+            <div className='bg-white/[0.4] h-[1px] mt-[20px] mb-[40px]'></div>
+        </div>
     </div>
   );
-}
+};
 
 export default App;
